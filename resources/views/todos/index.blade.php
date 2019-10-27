e5aa13af4e9adf6ea21a29593bdd81bdf64bd751
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-10-27 21:00:12 +0900

    add addTodoModal component

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -4,5 +4,9 @@
 <div class="container">
     <h1>Today's Todo!</h1>
 
+    <button type="button" class="btn btn-outline-primary" data-toggle="modal" data-target="#addTodoModal">Add Todo</button>
+
+    @component('todos.addTodoModal')
+    @endcomponent
 </div>
 @endsection
